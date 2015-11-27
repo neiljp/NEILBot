9a05838e6eb79436788bcb516ea8cb7549210e59
neiljp <github@kepier.clara.net>
2015-11-27 14:17:59 +0000

    Prefix tmr with alarm_ and move related config together, with descriptive comment

--- config_ircbot.lua
+++ config_ircbot.lua
@@ -4,7 +4,9 @@ irc.channels = {"#57Ntest","#57N"}
 irc.server = "chat.freenode.net"
 irc.port = 6667
 irc.join_message = nil
-irc.reconnect_time = 60 -- seconds
+
+irc.reconnect_time = 60 -- seconds before trying to reconnect to server
+irc.alarm_tmr = 3 -- alarm timer to use internally (to reconnect to server)
 
 irc.action_char = "~"
 
@@ -16,6 +18,5 @@ irc.actions["MAC"] = function(n) return wifi.sta.getmac() end
 irc.responses["^[Dd]ing$"]=function(n) return "DONG!" end
 irc.responses["^[Hh][iI] "..irc.nick]=function(n) return "Hi "..n.."!" end
 
-irc.tmr = 3
 irc.raw_server_messages = false -- Show messages sent from server? (aids debugging)
 irc.log = print -- Function to use to log messages (use print or define your own)
